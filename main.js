02f0a1398aba802bc5342b4133d378e48f6179f5
ricop7 <enricofpellini@gmail.com>
2023-08-31 14:27:22 -0400

    Update: Comments, Reformat

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// Need to add more features.
-// Redo some of the functions.
-
 const container = document.querySelector('.container');
 const searchButton = document.querySelector('.search-box button');
 const searchInput = document.getElementById('city-input');
@@ -10,10 +7,7 @@ const error404 = document.querySelector('.not-found');
 
 function getWeather(city){
     const APIKey = '5266d465d6b6e2f2dc7fb2a7735e6b0c';
-
-    
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
-
 
     .then(response => response.json())
     .then(json => {
@@ -62,6 +56,7 @@ function getWeather(city){
                 image.src = '';
         }
 
+            // HTML Text changes. 
         cityCountryElement.textContent = `${json.name}, ${json.sys.country}`;
         temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
         description.innerHTML = `${json.weather[0].description}`;
@@ -77,16 +72,16 @@ function getWeather(city){
     });
 }
 
+    // Click action.
 searchButton.addEventListener('click', ()=>{
     const city = searchInput.value;
 
     if (city === '')
     return;
-
     getWeather(city);
-
 })
 
+    // "Enter" key action.
 searchInput.addEventListener('keydown', event => {
     if (event.key === 'Enter') {
         const city = searchInput.value;
