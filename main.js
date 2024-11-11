0e1062e9f34225d7b1728c709f5898905bed4190
epellini <enricofpellini@gmail.com>
2024-11-11 18:34:40 -0500

    add IPinfo

--- main.js
+++ main.js
@@ -1,93 +1,101 @@
-const container = document.querySelector('.container');
-const searchButton = document.querySelector('.search-box button');
-const searchInput = document.getElementById('city-input');
-const weatherBox = document.querySelector('.weather-box');
-const weatherDetails = document.querySelector('.weather-details');
-const error404 = document.querySelector('.not-found');
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".container");
+  const searchButton = document.querySelector(".search-box button");
+  const searchInput = document.getElementById("city-input");
+  const weatherBox = document.querySelector(".weather-box");
+  const weatherDetails = document.querySelector(".weather-details");
+  const error404 = document.querySelector(".not-found");
+  let userCountry = "";
 
-function getWeather(city){
-    const APIKey = '5266d465d6b6e2f2dc7fb2a7735e6b0c';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+  function detectCountry() {
+    const geolocationKey = config.GEOLOCATION_API_KEY;
+    fetch(`https://ipinfo.io/json?token=${geolocationKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        userCountry = data.country;
+      })
+      .catch((error) => console.error("Geolocation API error:", error));
+  }
 
-    .then(response => response.json())
-    .then(json => {
+  function getWeather(city, country) {
+    const APIKey = config.WEATHER_API_KEY;
+    const location = country ? `${city},${country}` : city; // Use city, country if available
 
-        if (json.cod === '404') {
-            container.style.height = '400px';
-            weatherBox.style.display = 'none';
-            weatherDetails.style.display = 'none';
-            error404.style.display = 'block';
-            error404.classList.add('fadeIn');
-            return;
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${APIKey}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.cod === "404") {
+          container.style.height = "400px";
+          weatherBox.style.display = "none";
+          weatherDetails.style.display = "none";
+          error404.style.display = "block";
+          error404.classList.add("fadeIn");
+          return;
         }
 
-        error404.style.display = 'none';
-        error404.classList.remove('fadeIn');
+        error404.style.display = "none";
+        error404.classList.remove("fadeIn");
 
-        const cityCountryElement = document.querySelector('.city-country');
-        const image = document.querySelector('.weather-box img');
-        const temperature = document.querySelector('.weather-box .temperature');
-        const description = document.querySelector('.weather-box .description');
-        const humidity = document.querySelector('.weather-details .humidity span');
-        const wind = document.querySelector('.weather-details .wind span');
+        const cityCountryElement = document.querySelector(".city-country");
+        const image = document.querySelector(".weather-box img");
+        const temperature = document.querySelector(".weather-box .temperature");
+        const description = document.querySelector(".weather-box .description");
+        const humidity = document.querySelector(
+          ".weather-details .humidity span"
+        );
+        const wind = document.querySelector(".weather-details .wind span");
 
         switch (json.weather[0].main) {
-            case 'Clear':
-                image.src = 'images/clear.png';
-                break;
-
-            case 'Rain':
-                image.src = 'images/rain.png';
-                break;
-
-            case 'Snow':
-                image.src = 'images/snow.png';
-                break;
-
-            case 'Clouds':
-                image.src = 'images/cloud.png';
-                break;
-
-            case 'Haze':
-                image.src = 'images/mist.png';
-                break;
-
-            default:
-                image.src = '';
+          case "Clear":
+            image.src = "images/clear.png";
+            break;
+          case "Rain":
+            image.src = "images/rain.png";
+            break;
+          case "Snow":
+            image.src = "images/snow.png";
+            break;
+          case "Clouds":
+            image.src = "images/cloud.png";
+            break;
+          case "Haze":
+            image.src = "images/mist.png";
+            break;
+          default:
+            image.src = "";
         }
 
-            // HTML Text changes. 
         cityCountryElement.textContent = `${json.name}, ${json.sys.country}`;
         temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
         description.innerHTML = `${json.weather[0].description}`;
         humidity.innerHTML = `${json.main.humidity}%`;
         wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
-        weatherBox.style.display = '';
-        weatherDetails.style.display = '';
-        weatherBox.classList.add('fadeIn');
-        weatherDetails.classList.add('fadeIn');
-        container.style.height = '590px';
+        weatherBox.style.display = "";
+        weatherDetails.style.display = "";
+        weatherBox.classList.add("fadeIn");
+        weatherDetails.classList.add("fadeIn");
+        container.style.height = "590px";
+      });
+  }
 
-    });
-}
+  detectCountry();
 
-    // Click action.
-searchButton.addEventListener('click', ()=>{
+  searchButton.addEventListener("click", () => {
     const city = searchInput.value;
 
-    if (city === '')
-    return;
-    getWeather(city);
-})
+    if (city === "") return;
+    getWeather(city, userCountry);
+  });
 
-    // "Enter" key action.
-searchInput.addEventListener('keydown', event => {
-    if (event.key === 'Enter') {
-        const city = searchInput.value;
-        
-        if (city === '')
-        return;
-        getWeather(city);
+  searchInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const city = searchInput.value;
+
+      if (city === "") return;
+      getWeather(city, userCountry);
     }
-})
+  });
+});
